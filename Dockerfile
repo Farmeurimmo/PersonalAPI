df642c32559582b6470cea9ce6c0922359a82e8d
Farmeurimmo <farmeurimmo@gmail.com>
2023-09-04 09:11:32 +0200

    Better handling of redis connection lost

--- Dockerfile
+++ Dockerfile
@@ -3,11 +3,12 @@ FROM python:3.9
 WORKDIR /app
 
 ENV ENV_REDIS_HOST=localhost \
-    ENV_REDIS_PORT=6379
+    ENV_REDIS_PORT=6379 \
+    ENV_REDIS_DB=0 \
+    ENV_REDIS_PASSWORD=1234
 
 COPY . /app
 
 RUN pip install -r requirements.txt
 
-# Specify the command to run your project
 CMD ["python3", "main.py"]
--- RedisManager.py
+++ RedisManager.py
@@ -8,39 +8,57 @@ def connect_to_redis(host, port, password=None, db=0):
     return r
 
 
+def init_redis():
+    global redis_connection
+    try:
+        redis_connection = connect_to_redis(redis_host, redis_port, redis_password, redis_db)
+        print("Redis connection established")
+    except redis.exceptions.RedisError as e:
+        print(f"Error occurred while connecting to Redis: {e}")
+
+
+def get_redis_connection():
+    if redis_connection is None:
+        init_redis()
+    if redis_connection is None:
+        raise Exception("Redis connection is not established")
+    return redis_connection
+
+
 redis_host = os.getenv('REDIS_HOST') or 'localhost'
 redis_port = os.getenv('REDIS_PORT') or 6379
 redis_password = os.getenv('REDIS_PASSWORD') or None
 redis_db = os.getenv('REDIS_DB') or 0
 
-redis_connection = connect_to_redis(redis_host, redis_port, redis_password, redis_db)
+redis_connection = None
+init_redis()
 
 
 def get_value(key):
     try:
-        return redis_connection.get(key).decode('utf-8')
+        return get_redis_connection().get(key).decode('utf-8')
     except Exception:
         return None
 
 
 def set_value(key, value):
-    return redis_connection.set(key, value)
+    return get_redis_connection().set(key, value)
 
 
 def increment_value(key):
-    return redis_connection.incr(key)
+    return get_redis_connection().incr(key)
 
 
 def get_all_data(filter):
     try:
-        keys = redis_connection.keys('*')
+        keys = get_redis_connection().keys('*')
         new_keys = []
         for key in keys:
             if key.decode('utf-8').startswith(filter):
                 new_keys.append(key)
         data = {}
         for key in new_keys:
-            value = redis_connection.get(key).decode('utf-8')
+            value = get_redis_connection().get(key).decode('utf-8')
             data[key.decode('utf-8')] = value
         temp = data.copy()
         for key in temp.keys():
